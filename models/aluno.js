904053746d0d840f348a7ca17f85c5d2841bcd25
Samuel Soares <samuelsoares.academic@gmail.com>
2026-03-22 15:16:53 -0300

    Adicionando credenciais de email e senha para autenticar o usuário

--- models/aluno.js
+++ models/aluno.js
@@ -1,8 +1,11 @@
+// terceira modificação, adicionando credenciais de email e senha para autenticar o usuário
 let mongoose = require("mongoose");
 
 let alunoSchema = new mongoose.Schema({
   nome: { type: String, required: true },
   idade: { type: Number, required: true, min: 15, max: 99 },
+  email: { type: String, required: true, unique: true },
+  senha: { type: String, required: true },
   perfil: { type: mongoose.Schema.Types.ObjectId, ref: "Perfil" },
 });
 
